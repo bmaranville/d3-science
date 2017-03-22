1f22c397af314989238290726c6ff21f66dcbe6d
Brian Ben Maranville <brian.maranville@nist.gov>
2017-03-22 16:26:09 -0400

    adding linear response colormaps to colormap.js

--- make_colormaps_es6.py
+++ make_colormaps_es6.py
@@ -15,31 +15,24 @@ header = """\
 ////////////////////////////////////////
 // Colormaps borrowed from matplotlib //
 ////////////////////////////////////////
-
-var cm = (function() {
-  var colormap_data = 
+import * as d3 from 'd3'
+export const colormap_data =
 """
 
 footer = """
-  var colormap_names = Object.keys(colormap_data)
-  var x = [];
-  for (var i=0; i<256; i++) {
-    x[i] = i;
-  }
-  var get_colormap = function(cmap_name) {
-    var cmap = d3.scale.linear()
-      .domain(x)
-      .range(colormap_data[cmap_name])
-    return cmap
-  }
-  return {
-    colormap_data: colormap_data,
-    colormap_names: colormap_names,
-    get_colormap: get_colormap
-  }
-})();
+export const colormap_names = Object.keys(colormap_data);
+
+var x = d3.range(256);
+
+export function get_colormap(cmap_name) {
+  var cmap = d3.scale.linear()
+    .domain(x)
+    .range(colormap_data[cmap_name])
+  return cmap
+}
+
 """
-with open("get_colormap.js", "w") as outfile:
+with open("src/colormap.js", "w") as outfile:
     outfile.write(header)
     outfile.write(json.dumps(colors_dict))
     outfile.write(footer)
