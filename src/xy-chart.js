3e63d10ef0760f51f3d67ca3b4ad8994e3656f63
Travis CI <brian.maranville@nist.gov>
2017-06-20 16:00:10 +0000

    Deploy to GitHub Pages: ec9044e6f8fab88ded1d0994ccf0923b99e09003

--- src/xy-chart.js
+++ src/xy-chart.js
@@ -17,6 +17,7 @@ function xyChart(options_override) {
     show_points: true,
     show_line: true,
     show_errorbars: false,
+    show_grid: true,
     numberOfTicks: 4,
     position_cursor: true,
     vcursor: false,
@@ -684,6 +685,15 @@ function xyChart(options_override) {
     };
     
     chart.is_zoomed = function() { return is_zoomed; }
+    
+    chart.show_grid = function(_) {
+      if (!arguments.length) return options.show_grid;
+      options.show_grid = _;
+      chart.outercontainer.selectAll(".grid").style(
+        "display", (options.show_grid == true || options.show_grid == "true") ? "inline" : "none"
+      );
+      return chart;
+    };
     
     chart.x = function(_) {
       if (!arguments.length) return x;
